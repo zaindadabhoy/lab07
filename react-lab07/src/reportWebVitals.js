f894951682eb3ce4fc376d6a8fee7ad8680c7465
zaindadabhoy <zidadabhoy@gmail.com>
2024-12-23 14:05:04 +0500

    routing done

--- react-lab07/src/reportWebVitals.js
+++ react-lab07/src/reportWebVitals.js
@@ -1,3 +1,5 @@
+import { reportWebVitals } from 'web-vitals';
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
